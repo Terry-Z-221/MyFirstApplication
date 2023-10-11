9cd639b968f348cfed20236ab8fdce8a4be3c097
zengmz0221 <974378437@qq>
2023-10-11 10:55:41 +0800

    experiment4：Code for the MainActivity

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -2,10 +2,13 @@ package com.jnu.student;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.widget.RelativeLayout;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,21 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        RelativeLayout relativeLayout = new RelativeLayout(this);
-//        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-//                RelativeLayout.LayoutParams.WRAP_CONTENT,
-//                RelativeLayout.LayoutParams.WRAP_CONTENT);
-//        //addRule参数对应RelativeLayout的XML布局属性
-//        params.addRule(RelativeLayout.CENTER_IN_PARENT);                    //设置居中显示
-//
-//        //TextView textView = new TextView(this);                     //创建TextView控件
-//        TextView textView = findViewById(R.id.text_view_hello_world);       //通过id找到view中的TextView并获得对应的对象
-//
-//        textView.setText(getResources().getString(R.string.JavaForActivity));  //设置TextView控件的文字内容
-//        textView.setTextColor(Color.RED);                                   //设置TextView控件的文字颜色
-//        textView.setTextSize(18);                                           //设置TextView控件的文字大小
-//        //添加TextView对象和TextView的布局属性
-//        relativeLayout.addView(textView, params);
-//        setContentView(relativeLayout);
+
+    }
+    public void changeText(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("交换文本"); // 设置弹出窗口标题
+        builder.setMessage("确定要交换文本吗？"); // 设置弹出窗口消息内容
+        builder.setPositiveButton("确定", (dialog, which) -> {
+            // 点击确定按钮后的操作
+
+            //更换文本
+            TextView textViewL = findViewById(R.id.textViewLeft);
+            TextView textViewR = findViewById(R.id.textViewRight);
+            if (getResources().getString(R.string.text_Hello) == textViewL.getText()) {
+                textViewL.setText(R.string.text_JNU);
+                textViewR.setText(R.string.text_Hello);
+            } else {
+                textViewL.setText(R.string.text_Hello);
+                textViewR.setText(R.string.text_JNU);
+            }
+            //弹出提醒消息
+            Toast toast = Toast.makeText(getApplicationContext(), "交换成功！", Toast.LENGTH_SHORT);
+            toast.show();
+        });
+        builder.setNegativeButton("取消", (dialog, which) -> {
+            // 点击取消按钮后的操作
+
+            //弹出提醒消息
+            Toast toast = Toast.makeText(getApplicationContext(), "操作取消成功!", Toast.LENGTH_SHORT);
+            toast.show();
+        });
+        builder.show(); // 显示弹出窗口
     }
 }
