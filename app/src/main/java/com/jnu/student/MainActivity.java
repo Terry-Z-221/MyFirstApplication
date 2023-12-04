b51448392b82f16c2c7fa58d39f9b00f36fad496
zengmz0221 <974378437@qq>
2023-12-04 10:54:44 +0800

    experiment6：Entire Code

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -1,13 +1,19 @@
 package com.jnu.student;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+import com.jnu.student.data.Book;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,38 +21,75 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.activity_main_6);
+
+        RecyclerView recyclerView = findViewById(R.id.recycle_view_books);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));   //设置布局管理器
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book("软件项目管理案例教程（第4版）", R.drawable.book_2));
+        books.add(new Book("book", R.drawable.book_no_name));
+        books.add(new Book("信息安全数学基础（第2版）", R.drawable.book_1));
+
+        RecycleViewBookAdapter recycleViewBookAdapter = new RecycleViewBookAdapter(books);
+        recyclerView.setAdapter(recycleViewBookAdapter);
     }
-    public void changeText(View view) {
+    public static class RecycleViewBookAdapter extends RecyclerView.Adapter<RecycleViewBookAdapter.ViewHolder> {
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("交换文本"); // 设置弹出窗口标题
-        builder.setMessage("确定要交换文本吗？"); // 设置弹出窗口消息内容
-        builder.setPositiveButton("确定", (dialog, which) -> {
-            // 点击确定按钮后的操作
+        private final ArrayList<Book> localDataSet;
 
-            //更换文本
-            TextView textViewL = findViewById(R.id.textViewLeft);
-            TextView textViewR = findViewById(R.id.textViewRight);
-            if (getResources().getString(R.string.text_Hello) == textViewL.getText()) {
-                textViewL.setText(R.string.text_JNU);
-                textViewR.setText(R.string.text_Hello);
-            } else {
-                textViewL.setText(R.string.text_Hello);
-                textViewR.setText(R.string.text_JNU);
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            private final TextView bookTitle;
+            private final ImageView bookCover;
+
+            public TextView getBookTitle() {
+                return bookTitle;
             }
-            //弹出提醒消息
-            Toast toast = Toast.makeText(getApplicationContext(), "交换成功！", Toast.LENGTH_SHORT);
-            toast.show();
-        });
-        builder.setNegativeButton("取消", (dialog, which) -> {
-            // 点击取消按钮后的操作
 
-            //弹出提醒消息
-            Toast toast = Toast.makeText(getApplicationContext(), "操作取消成功!", Toast.LENGTH_SHORT);
-            toast.show();
-        });
-        builder.show(); // 显示弹出窗口
+            public ImageView getBookCover() {
+                return bookCover;
+            }
+            public ViewHolder(View view) {
+                super(view);
+                // Define click listener for the ViewHolder's View
+
+                bookTitle = (TextView) view.findViewById(R.id.text_view_book_title);
+                bookCover = (ImageView) view.findViewById(R.id.image_view_book_cover);
+            }
+
+        }
+
+        public RecycleViewBookAdapter(ArrayList<Book> dataSet) {
+            localDataSet = dataSet;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            // Create a new view, which defines the UI of the list item
+            View view = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.book_item_row, viewGroup, false);
+
+            return new ViewHolder(view);
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+            // Get element from your dataset at this position and replace the
+            // contents of the view with that element
+            viewHolder.getBookTitle().setText(localDataSet.get(position).getTitle());
+            viewHolder.getBookCover().setImageResource(localDataSet.get(position).getCoverResourceId());
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return localDataSet.size();
+        }
     }
+
 }
---
+++ app/src/main/java/com/jnu/student/data/Book.java
@@ -0,0 +1,20 @@
+package com.jnu.student.data;
+
+public class Book {
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCoverResourceId() {
+        return coverResourceId;
+    }
+
+    private final String title;
+    private final int coverResourceId;
+
+    public Book(String title_, int coverResourceId_) {
+        this.title = title_;
+        this.coverResourceId = coverResourceId_;
+    }
+}
